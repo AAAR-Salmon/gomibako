6a5ded7dfc538bc99eac4e63023b7a9c62571cf3
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2023-06-20 11:21:07 +0900

    Fix blocking sleep (tokio_call)

--- tokio_call/src/main.rs
+++ tokio_call/src/main.rs
@@ -1,12 +1,8 @@
-use std::{
-    fs::File,
-    io::{stdout, Write},
-    thread,
-    time::{self, Duration},
-};
+use std::fs::File;
+use std::io::{stdout, Write};
+use std::time::{self, Duration};
 
 use serde::{Deserialize, Serialize};
-
 use tokio;
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -17,22 +13,22 @@ struct Member {
 }
 
 impl Member {
-    fn introduce(&self) {
+    async fn introduce(&self) {
         let str = format!(
             "{}，{}歳❗ {}担当です❗❗",
             &self.name, &self.age, &self.instrument
         );
         let dur = time::Duration::from_millis(100);
-        typewriter(str, dur);
+        typewriter(str, dur).await;
     }
 }
 
-fn typewriter(str: String, dur: Duration) {
+async fn typewriter(str: String, dur: Duration) {
     let mut stdout = stdout();
     for c in str.chars() {
         print!("{}", c);
         stdout.flush().unwrap();
-        thread::sleep(dur);
+        tokio::time::sleep(dur).await;
     }
     println!();
 }
@@ -41,14 +37,16 @@ fn typewriter(str: String, dur: Duration) {
 async fn main() {
     let file = File::open("members.yml").unwrap();
     let members: Vec<Member> = serde_yaml::from_reader(file).unwrap();
-    let mut tasks = Vec::new();
+
+    let mut tasks = Vec::with_capacity(members.len());
     for (i, member) in members.iter().enumerate() {
-        let member = (*member).clone();
-        tasks.push(tokio::spawn(async move {
+        let member = member.clone();
+        let task = tokio::spawn(async move {
             let sec = time::Duration::from_secs(i.try_into().unwrap());
-            thread::sleep(sec);
-            member.introduce();
-        }));
+            tokio::time::sleep(sec).await;
+            member.introduce().await;
+        });
+        tasks.push(task);
     }
 
     for task in tasks {
